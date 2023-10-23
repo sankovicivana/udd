9b20e114f37b1aa54c5aad7693a98d2015ecb3a6
Ivana Sekereš <67102046+sankovicivana@users.noreply.github.com>
2023-10-23 19:42:45 +0000

    init spring boot

--- project/src/main/java/com/udd/project/ProjectApplication.java
+++ project/src/main/java/com/udd/project/ProjectApplication.java
@@ -2,12 +2,26 @@ package com.udd.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProjectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectApplication.class, args);
+    System.out.println("Project started");
   }
 
+}
+
+@RestController
+class MyController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
--- project/src/test/java/com/udd/project/ProjectApplicationTests.java
+++ project/src/test/java/com/udd/project/ProjectApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
