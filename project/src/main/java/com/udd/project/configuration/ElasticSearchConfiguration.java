7afaa85135fb5cf18c59223240213266725cebec
sankovicivana <sankovicivana27@gmail.com>
2023-12-20 00:22:11 +0100

    gitignore added

--- project/src/main/java/com/udd/project/configuration/ElasticSearchConfiguration.java
+++ project/src/main/java/com/udd/project/configuration/ElasticSearchConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 
-
-
 @Configuration
 public class ElasticSearchConfiguration  extends org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration {
     @Value("${elasticsearch.host}")
